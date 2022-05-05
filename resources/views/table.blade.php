ce1abc1e19b24ac1ae03d398b4452101c8375b75
Dmitrij Kunts <r206265@gmail.com>
2022-05-05 19:29:22 +0300

    test done

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,14 +1,22 @@
-<!-- TODO Blade Задание 2: Изменить реализацию этой view, расширить ее с использованием layout  -->
-<!-- layouts/app.blade.php  -->
+@extends('layouts.app')
+
+@section('content')
+    <!-- TODO Blade Задание 2: Изменить реализацию этой view, расширить ее с использованием layout  -->
+    <!-- layouts/app.blade.php  -->
 
 
-<!-- TODO Blade Задание 6: В эту view с контроллера передается collection c users в переменной data -->
-<!-- Выполнить foreach loop в одну строку -->
-<!-- Используйте view shared/user.blade.php для item (переменная user во item view) -->
-<!-- Используйте view shared/empty.blade.php для состояния когда нет элементов в колекции -->
+    <!-- TODO Blade Задание 6: В эту view с контроллера передается collection c users в переменной data -->
+    <!-- Выполнить foreach loop в одну строку -->
+    <!-- Используйте view shared/user.blade.php для item (переменная user во item view) -->
+    <!-- Используйте view shared/empty.blade.php для состояния когда нет элементов в колекции -->
+    @forelse ($data as $user)
+        @include('shared.user', compact('user'))
+    @empty
+        @include('shared.empty')
+    @endforelse
 
 
-<!-- TODO Blade Задание 7: Здесь сделайте классический foreach loop -->
-<!-- Выведите div с $user->name -->
-<!-- Воспользуйтесь переменной $loop и у нечетных div выведите класс - bg-red-500 -->
-
+    <!-- TODO Blade Задание 7: Здесь сделайте классический foreach loop -->
+    <!-- Выведите div с $user->name -->
+    <!-- Воспользуйтесь переменной $loop и у нечетных div выведите класс - bg-red-500 -->
+@endsection
