972119a206a9f80d3e46d7a6b0921132e755c6bd
twent <twentprod@gmail.com>
2022-05-05 14:05:03 +0300

    6. Auth Policies

--- app/Policies/ItemPolicy.php
+++ app/Policies/ItemPolicy.php
@@ -13,7 +13,6 @@ class ItemPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function viewAny(User $user)
@@ -24,8 +23,6 @@ class ItemPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Item  $item
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function view(User $user, Item $item)
@@ -36,21 +33,17 @@ class ItemPolicy
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function create(User $user)
     {
         // TODO Auth Задание: Разрешить добавление продуктов только пользователю с id = 10
-
-        return true;
+        return $user->id === 10;
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Item  $item
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function update(User $user, Item $item)
@@ -61,8 +54,6 @@ class ItemPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Item  $item
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function delete(User $user, Item $item)
@@ -73,8 +64,6 @@ class ItemPolicy
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Item  $item
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function restore(User $user, Item $item)
@@ -85,8 +74,6 @@ class ItemPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Item  $item
      * @return \Illuminate\Auth\Access\Response|bool
      */
     public function forceDelete(User $user, Item $item)
