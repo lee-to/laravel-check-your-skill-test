e38279caec6ed681c6df5e317c2dad1d03bac7be
Ruslan <kandibaruslan@gmail.com>
2022-10-07 14:01:14 +0400

    Tests: 32 passed

--- database/migrations/tasks/2021_11_18_122318_create_posts_table.php
+++ database/migrations/tasks/2021_11_18_122318_create_posts_table.php
@@ -16,29 +16,50 @@ class CreatePostsTable extends Migration
         //TODO Migrations Задание 1: Создать таблицу categories с 2 полями id и title (не забыть про timestamps)
         //
 
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
 
             //TODO Migrations Задание 2: Для title указать что значение по умолчанию NULL
-
+            $table->string('title')->nullable();
             //TODO Migrations Задание 3: Для active указать что значение по умолчанию TRUE
-
+            $table->boolean('active')->default(true);
             //TODO Migrations Задание 4: Добавить функционал soft delete
-
+            $table->softDeletes();
             //TODO Migrations Задание 5: Добавить поля с timestamps (created_at, updated_at) через 1 метод
+            $table->timestamps();
         });
 
         Schema::table('posts', function (Blueprint $table) {
             //TODO Migrations Задание 6: Добавить поле description типа text (DEFAULT NULL) ПОСЛЕ поля title
-
+            $table->text('description')->nullable()->after('title');
             //TODO Migrations Задание 7: Сделать провеку на наличие поля active и в случаи успеха добавить поле main (boolean default false)
-
+            if (Schema::hasColumn('posts', 'active'))
+            {
+                Schema::table('posts', function (Blueprint $table) {
+                    $table->boolean('main')->default(false);
+                });
+            };
             //TODO Migrations Задание 8: Переименовать поле title в name
+            $table->renameColumn('title', 'name');
         });
 
         //TODO Migrations Задание 9: Переименовать таблицу posts в articles
+        Schema::rename('posts', 'articles');
 
         //TODO Migrations Задание 10: Добавить таблицу для связи articles и categories (belongsToMany) c foreign ключами
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->unsignedBigInteger('article_id')->index();
+            $table->foreign('article_id')->references('id')->on('articles')->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->unsignedBigInteger('category_id')->index();
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+        });
     }
 
     /**
@@ -49,5 +70,8 @@ class CreatePostsTable extends Migration
     public function down()
     {
         // TODO Migrations Задание 11: Удалить таблицы categories, articles, article_category если такие существуют
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('article_category');
     }
 }
