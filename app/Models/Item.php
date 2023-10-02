531cf8f699c42e703c450d48311260516baf9f43
Timofey Mukhin <muhin@glos-trust.com>
2023-10-02 16:58:06 +0700

    test complete!

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,13 @@ class Item extends Model
     protected $fillable = ['title', 'active'];
 
     // TODO Eloquent Задание 1: указать что таблица - products
+    protected $table = 'products';
+
+    /**
+     * Scope a query to only include users of a given type.
+     */
+    public function scopeActive(Builder $query):void
+    {
+        $query->where('active',1);
+    }
 }
--- database/migrations/tasks/2021_11_18_122318_create_posts_table.php
+++ database/migrations/tasks/2021_11_18_122318_create_posts_table.php
@@ -15,9 +15,18 @@ class CreatePostsTable extends Migration
     {
         //TODO Migrations Задание 1: Создать таблицу categories с 2 полями id и title (не забыть про timestamps)
         //
+        Schema::create('categories', function (Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
 
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable()->default(null);
+            $table->boolean('active')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
 
             //TODO Migrations Задание 2: Для title указать что значение по умолчанию NULL
 
@@ -30,15 +39,31 @@ class CreatePostsTable extends Migration
 
         Schema::table('posts', function (Blueprint $table) {
             //TODO Migrations Задание 6: Добавить поле description типа text (DEFAULT NULL) ПОСЛЕ поля title
-
-            //TODO Migrations Задание 7: Сделать провеку на наличие поля active и в случаи успеха добавить поле main (boolean default false)
-
-            //TODO Migrations Задание 8: Переименовать поле title в name
+            $table->addColumn('text', 'description')->after('title')->default(null);
         });
 
+        //TODO Migrations Задание 7: Сделать провеку на наличие поля active и в случаи успеха добавить поле main (boolean default false)
+        if (Schema::hasColumn('posts', 'active')) {
+            Schema::table('posts', function (Blueprint $table){
+                $table->addColumn('boolean','main')->default(false);
+            });
+        }
+        //TODO Migrations Задание 8: Переименовать поле title в name
+
+        Schema::table('posts', function (Blueprint $table){
+            $table->renameColumn('title','name');
+        });
+
+
         //TODO Migrations Задание 9: Переименовать таблицу posts в articles
+        Schema::rename('posts', 'articles');
 
         //TODO Migrations Задание 10: Добавить таблицу для связи articles и categories (belongsToMany) c foreign ключами
+        Schema::create('article_category', function (Blueprint $table){
+            $table->id();
+            $table->foreignId('article_id');
+            $table->foreignId('category_id');
+        });
     }
 
     /**
@@ -49,5 +74,9 @@ class CreatePostsTable extends Migration
     public function down()
     {
         // TODO Migrations Задание 11: Удалить таблицы categories, articles, article_category если такие существуют
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('articles');
+        Schema::dropIfExists('article_category');
     }
 }
