254ec38a802a878b99575f3416b67692954fef32
work3212 <48941393+work3212@users.noreply.github.com>
2023-10-02 16:37:24 +0300

    Blade

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,14 +1,21 @@
 <!-- TODO Blade Задание 2: Изменить реализацию этой view, расширить ее с использованием layout  -->
 <!-- layouts/app.blade.php  -->
-
+@extends('layouts.app')
 
 <!-- TODO Blade Задание 6: В эту view с контроллера передается collection c users в переменной data -->
 <!-- Выполнить foreach loop в одну строку -->
 <!-- Используйте view shared/user.blade.php для item (переменная user во item view) -->
 <!-- Используйте view shared/empty.blade.php для состояния когда нет элементов в колекции -->
-
+@each('shared.user', $data, 'user', 'shared.empty')
 
 <!-- TODO Blade Задание 7: Здесь сделайте классический foreach loop -->
 <!-- Выведите div с $user->name -->
 <!-- Воспользуйтесь переменной $loop и у нечетных div выведите класс - bg-red-500 -->
-
+@foreach($data as $user)
+    <div @if($loop->odd)
+         class="bg-red-500"
+        @endif
+    >
+        {{$user->name}}
+    </div>
+@endforeach
